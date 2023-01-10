6004ff6399a773b45567354ab3e85d9f1f700509
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-01-10 20:16:41 -0300

    Add Frontmatter interface

---
+++ src/types/IFrontmatter.ts
@@ -0,0 +1,14 @@
+interface IImage {
+  alt: string;
+  url: string;
+}
+
+export default interface IFrontmatter {
+  title: string;
+  image: IImage;
+  pubDate: string;
+  description: string;
+  author: string;
+  tags: string[];
+  draft?: boolean;
+}
