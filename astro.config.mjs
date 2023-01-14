a56da4e487e18388bb585fa4db421c76da639620
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-01-14 20:07:24 -0300

    Add sample static endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
+import yaml from "@rollup/plugin-yaml";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [yaml()],
+  },
   integrations: [preact()],
   markdown: {
     shikiConfig: {
---
+++ src/pages/api/[route]/[id].json.ts
@@ -0,0 +1,20 @@
+// http://127.0.0.1:3000/api/heroes/2.json
+import type { APIRoute } from "astro";
+import data from "../../../../content/data/api.json";
+
+export const get: APIRoute = ({ params, request }) => {
+  const { route, id } = params;
+  return {
+    body: JSON.stringify(
+      data.filter((item) => item.id === id && item.route === route)[0]
+    ),
+  };
+};
+
+export function getStaticPaths() {
+  return [
+    { params: { route: "heroes", id: "0" } },
+    { params: { route: "heroes", id: "1" } },
+    { params: { route: "heroes", id: "2" } },
+  ];
+}
