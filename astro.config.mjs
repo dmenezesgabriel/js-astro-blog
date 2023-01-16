3c689d5a38c7be423669ed5878db1ab5362db95c
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-01-16 19:45:56 -0300

    Add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
 import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [yaml()],
   },
-  integrations: [preact()],
+  integrations: [preact(), mdx()],
   markdown: {
     shikiConfig: {
       theme: "dracula",
