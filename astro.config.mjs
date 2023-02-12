b41e968aaccca28120578e222f0a2d2b52d3cb9f
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-02-12 10:52:25 -0300

    add env template and env types

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dmenezesgabriel.github.io",
-  base: import.meta.env.PROD === true ? "./" : "/",
+  base:
+    import.meta.env.PROD === true ? import.meta.env.VITE_PROD_BASE_URL : "/",
   vite: {
     plugins: [yaml()],
   },
