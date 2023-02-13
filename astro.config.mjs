91fec9b3464b04dafc7a0ee497d341da550e173f
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-02-13 20:03:53 -0300

    Update base to use environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.VITE_PROD_SITE_URL,
-  base: import.meta.env.PROD === true ? "/astro-blog" : "/",
+  base:
+    import.meta.env.PROD === true ? import.meta.env.VITE_PROD_BASE_URL : "/",
   vite: {
     plugins: [yaml()],
   },
