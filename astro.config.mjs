4475ded38b08d9bd9aecc3e5f72938e1415ab83b
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-02-13 19:33:45 -0300

    change site path to environment variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dmenezesgabriel.github.io/",
+  site: import.meta.env.VITE_PROD_SITE_URL,
   base:
     import.meta.env.PROD === true ? import.meta.env.VITE_PROD_BASE_URL : "/",
   vite: {
